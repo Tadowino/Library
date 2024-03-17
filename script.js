e76a6608d70f14dc3a3a6fd8d65c2448c8a92f35
Tadowino <tadowino254@gmail.com>
2024-03-17 18:58:20 +0300

    display form style form to display in block overiding the html styling

---
+++ script.js
@@ -0,0 +1,28 @@
+const myLibrary = [];
+
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+}
+
+function addBookToLibrary() {
+  let title = document.querySelector("#title").value;
+  let author = document.querySelector("#author").value;
+  let pages = document.querySelector("#pages").value;
+  let read = document.querySelector("#read").value;
+  let newBook = Book(title, author, pages, read).checked;
+  console.log(newBook);
+}
+
+let newBookBtn = document.querySelector("#new-book-btn");
+newBookBtn.addEventListener("click",function(){
+  let newBookForm = document.querySelector("#new-book-form");
+  newBookForm.style.display = "block";
+})
+
+document.querySelector("#new-book-form").addEventListener("submit",function(event){
+  event.preventDefault();
+  addBookToLibrary();
+})
